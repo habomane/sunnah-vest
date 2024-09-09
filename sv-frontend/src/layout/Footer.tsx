f4bf470639e6329e3a02719578effd94e1b2e5ea
omaneh <122321976+omaneh@users.noreply.github.com>
2024-09-08 22:07:58 -0400

    Began working on layout.

---
+++ sv-frontend/src/layout/Footer.tsx
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+      <footer className=" py-6 text-md font-medium text-center">
+        <p className="uppercase tracking-wide"><span className="text-green-700">Community</span> Library</p>
+        <p className="font-normal">2024 &copy; All Rights Reserved </p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
